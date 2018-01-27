c82225ab820c6ef940354488ce79f10c2d9cefc8
Dan Colman <danrcolman@gmail.com>
2018-01-26 20:50:31 -0800

    more changes

--- Transmission/Assets/Cell.cs
+++ Transmission/Assets/Cell.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour {
 
-    private bool isEvil;
-    private float lowResponseFreq;
-    private float hiResponseFreq;
-    private int health = 5;
+    public bool isEvil = true;
+    public float loResponseFreq;
+    public float hiResponseFreq;
+    public int health = 5;
 
+    private Vector2 position;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if(health <= 0) {
             // Play death animation
-            // Die, Destroy(this,1);
+            // Destroy(this,1);
         }
 
-        if(frequencty >= lowResponseFreq && frequency <= hiResponseFreq) {
+        var frequency = 100; // Get this from the current input
+
+        if(frequency >= loResponseFreq && frequency <= hiResponseFreq) {
+            StartVibrating();
             health--;
+        } else {
+            StopVibrating();
         }
 	}
+
+    void StartVibrating() {
+        // Scale the cell big and small if it's in the response range
+    }
+
+    void StopVibrating() {
+        // transform back to 1:1 scale;
+    }
 }
--- Transmission/Assets/Game.cs
+++ Transmission/Assets/Game.cs
@@ -4,13 +4,66 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
+    private List<Cell> Cells;
+    private int loFrequency;
+    private int hiFrequency;
+    private int difficulty = 100; // Set the difference of the frequency response range offset between good/evil Cells
+    private int responseVariance = 100;
+
 	// Use this for initialization
 	void Start () {
-		
+        // Init the playfield
+        // Populate with 100 or so Cells
+        Cells.Add(CreateGoodCell());
+
+        // These are the response ranges
+        loFrequency = 100;
+        hiFrequency = 4000;
+
+        // randomize their position
+        // create the Cells (51%+ evil)
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        // Get the X position of the mouse
+        var frequency = Input.mousePosition.x;
+
+
+        // Get count of cells where gameObject.tag == 'Enemy'
+		// if Cells.isEvil = true == 0?
+        // You win!
+
+        // if Cells.isEvil = false == 0?
+        // You lose!
 	}
+
+    private Cell CreateGoodCell() {
+        //instantiate Cell
+        var c = new Cell();
+        c.tag = "Player";
+        c.isEvil = false;
+
+        // set the response range
+        c.loResponseFreq = loFrequency - responseVariance - (Random.value * responseVariance);
+        c.hiResponseFreq = hiFrequency - responseVariance + (Random.value * responseVariance);
+
+        return c;
+        
+    }
+
+    private Cell CreateBadCell()
+    {
+        //instantiate Cell
+        var c = new Cell();
+        c.tag = "Enemy";
+
+        // set the response range
+        c.loResponseFreq = loFrequency + responseVariance - (Random.value * responseVariance);
+        c.hiResponseFreq = hiFrequency + responseVariance + (Random.value * responseVariance);
+
+        return c;
+
+    }
 }
