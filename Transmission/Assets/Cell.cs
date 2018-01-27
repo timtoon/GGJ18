85168fbe7aab280c930d46e7cb53433479beb6f9
Michael Pelz <mpelz.lill@gmail.com>
2018-01-27 03:30:28 -0800

    Updated for population locations of cells. All is held within CellCreationsAndDying prefab. Just add it to the scene.  (Not adding scene, all within the prefab.)

--- Transmission/Assets/Cell.cs
+++ Transmission/Assets/Cell.cs
@@ -6,31 +6,44 @@ public class Cell : MonoBehaviour {
 
 
 	private delegate void StatePointer();
-	private static StatePointer currentState;
+	private StatePointer currentState;
 
     public bool isEvil = true;
 	public float health = 5f;
 
-	private float loResponseFreq;
-    private float hiResponseFreq;
-	private float maxHealth;
+	public float loResponseFreq;
+    public float hiResponseFreq;
+	public float maxHealth = 5f;
+	public float maxSize = 5f;
+	// Original Color
+	private Color OriginalColor;
 
-    private Vector2 position;
 
 
+
+	//------------------------- System functions.
 	// Use this for initialization
 	void Start () 
 	{
+
+		// Testing only///
+		OriginalColor = gameObject.GetComponent<SpriteRenderer>().color;
+
+
+
 		maxHealth = health;
 		//currentState = isOK;
 		//currentState = new StatePointer(isOK);
 		setStateOk();
+
+		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		currentState();
+
 		/*
         if(health <= 0) {
             // Play death animation
@@ -48,19 +61,48 @@ public class Cell : MonoBehaviour {
 		*/
 	}
 
-    void IsHurting()
+	// ------------------- Object updates...
+	void setFrequency(float LowFrequency, float HighFrequency)
+	{
+		loResponseFreq = LowFrequency;
+		hiResponseFreq = HighFrequency;
+	}
+
+	//------------------------------ States
+
+	private void IsHurting()
 	{
 		health = health - Time.deltaTime;
 		if (health < 0)
+		{
 			die();
+			gameObject.active = false;
+			//Destroy(gameObject);
+		}
+		else
+		{
+			setSize((1 - (health / maxHealth)) * maxSize + 1);
+		}
 	}
-
-    void isOK()
+	private void setSize(float size)
 	{
-		health = health + Time.deltaTime;
+		gameObject.transform.localScale = new Vector3(size, size);
 	}
 
-    void Activate(float newFrequency)
+    private void isOK()
+	{
+		//health = health + Time.deltaTime;
+		health = (health < maxHealth) ? health + Time.deltaTime : maxHealth;
+		setSize((1 - (health / maxHealth)) * maxSize + 1);  
+	}
+	public void dead()
+	{
+
+	}
+
+	//--------------------------------- Interaction
+
+	public void Activate(float newFrequency)
 	{
 		if (newFrequency >= loResponseFreq && newFrequency <= hiResponseFreq)
 		{
@@ -72,21 +114,31 @@ public class Cell : MonoBehaviour {
 		}
 	}
 
-    void setStateDying()
+	//---------------------------------------- Changes of States.
+    public void setStateDying()
 	{
 		currentState = IsHurting;
+		print("I'm now hurting");
+
+		// Testing only....
+		gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
         // Scale the cell big and small if it's in the response range
 	}
 
-    void setStateOk()
+    public void setStateOk()
 	{
 		currentState = isOK;
-        // transform back to 1:1 scale;
+		print("I'm now OK");
+		// Testing only....
+		gameObject.GetComponent<SpriteRenderer>().color = OriginalColor;
+
+		// transform back to 1:1 scale;
 	}
 
-    void die()
+	public void die()
 	{
-		Game.instance.SetDie(isEvil);
-		Destroy(gameObject);
+		currentState = dead;
+		Game.instance.SetDie(this.gameObject);
+		//Destroy(gameObject);
 	}
 }
--- Transmission/Assets/Game.cs
+++ Transmission/Assets/Game.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+using System.Linq;
+
 public class Game : MonoBehaviour 
 {
 	public static Game instance;
-	public int GoodCells;
-	public int BadCells;
 
-    private List<Cell> Cells;
+	
+
+	public int GoodCells;
+	//public float GoodCellHealth;
+	public GameObject GoodCellObject;
+
+	public int BadCells;
+	//public float BadCellHealth;
+	public GameObject BadCellObject;
+
+	public float radius;
+
+	private List<GameObject> Cells = new List<GameObject>();
     private int loFrequency;
     private int hiFrequency;
-    private int difficulty = 100; // Set the difference of the frequency response range offset between good/evil Cells
+
+
+    //private int difficulty = 100; // Set the difference of the frequency response range offset between good/evil Cells
     private int responseVariance = 100;
+	private float LastFrequency;
+	
 
     private void Awake()
 	{
@@ -21,15 +38,17 @@ public class Game : MonoBehaviour
 			Destroy(gameObject);
 		}
 		instance = this;
-		DontDestroyOnLoad(instance);
+		GameObject.DontDestroyOnLoad(instance);
+		
 	}
 
     // Use this for initialization
 	void Start () 
 	{
-        // Init the playfield
-        // Populate with 100 or so Cells
-        Cells.Add(CreateGoodCell());
+		// Init the playfield
+		// Populate with 100 or so Cells
+		populateDish(); 
+		//Cells.Add(CreateGoodCell());
 
         // These are the response ranges
         loFrequency = 100;
@@ -38,27 +57,69 @@ public class Game : MonoBehaviour
         // randomize their position
         // create the Cells (51%+ evil)
 	}
-	
+
+
+	//----------------------------------------- Input Handlers
+
 	// Update is called once per frame
 	void Update () 
 	{
 
         // Get the X position of the mouse
-        var frequency = Input.mousePosition.x;
+        float frequency = Input.mousePosition.x;
+		//frequency = 5;
+		if (frequency != LastFrequency)
+		{
+			GameObject.Find("Text").GetComponent<UnityEngine.UI.Text>().text = "" +  frequency;
+			print("Updating Frequency");
+			InputDataUpdateFrequency(frequency);
+			LastFrequency = frequency;
+		}
+		
 
 
-        // Get count of cells where gameObject.tag == 'Enemy'
-        // if Cells.isEvil = true == 0?
-        // You win!
+		// Get count of cells where gameObject.tag == 'Enemy'
+		// if Cells.isEvil = true == 0?
+		// You win!
 
-        // if Cells.isEvil = false == 0?
-        // You lose!
+		// if Cells.isEvil = false == 0?
+		// You lose!
 	}
 
-    public void SetDie(bool isEvil)
+
+
+
+	public void InputDataUpdateFrequency(float frequency)
 	{
-		if (isEvil)
+		//using System.Linq;
+		//int max = Cells.Count;
+		for (int x = 0; x < Cells.Count; x++)
 		{
+			GameObject C = Cells.ElementAt<GameObject>(x);
+			Cell myCell = C.GetComponent<Cell>();
+			myCell.Activate(frequency);
+		}
+			//Cell C = Cells[x].get<Cell>();
+
+	}
+
+
+
+
+
+
+
+
+
+	//----------------------------------------- End of game Checking
+
+
+	public void SetDie(GameObject DeadCell)
+	{
+		
+		if (DeadCell.GetComponent<Cell>().isEvil)
+		{
+			print("Bad Died");
 			BadCells--;
 			if (BadCells <= 0)
 			{
@@ -67,12 +128,15 @@ public class Game : MonoBehaviour
 		}
 		else
 		{
+			print("Good Died");
 			GoodCells--;
 			if (GoodCells <= 0)
 			{
 				loseLevel();
 			}
 		}
+		Cells.Remove(DeadCell);
+		//Cells.FindIndex(DeadCell)
 	}
 	public void winLevel()
 	{
@@ -83,32 +147,86 @@ public class Game : MonoBehaviour
 		print("You lost the level");
 	}
 
-    private Cell CreateGoodCell()
+
+
+
+
+
+
+
+
+
+	//----------------------------------------- POPULATIONS
+
+	public void populateDish()
+	{
+		for (int x = 0; x < GoodCells; x++)
+		{
+			Cells.Add(CreateGoodCell());
+			print("Add Good Cells");
+		}
+		for (int x = 0; x < BadCells; x++)
+		{
+			Cells.Add(CreateBadCell());
+			print("Add Bad Cells");
+		}
+	}
+
+    private GameObject CreateGoodCell()
     {
-        //instantiate Cell
-        var c = new Cell();
-        c.tag = "Player";
+		GameObject myCell = Instantiate(GoodCellObject);
+		//instantiate Cell
+		//var c = new Cell();
+		myCell.tag = "Player";
+		Cell c = myCell.GetComponent<Cell>();
         c.isEvil = false;
 
-        // set the response range
+
+
+
+		
+		// set the response range
+		
         c.loResponseFreq = loFrequency - responseVariance - (Random.value * responseVariance);
         c.hiResponseFreq = hiFrequency - responseVariance + (Random.value * responseVariance);
 
-        return c;
+		placeCell(myCell);
+        return myCell;
 
     }
 
-    private Cell CreateBadCell()
+
+	private GameObject CreateBadCell()
     {
-        //instantiate Cell
-        var c = new Cell();
-        c.tag = "Enemy";
+		GameObject myCell = Instantiate(BadCellObject);
+		//var c = new Cell();
+		Cell c = myCell.GetComponent<Cell>();
+		//c.isEvil = true;
+		//c.tag = "Enemy";
 
         // set the response range
         c.loResponseFreq = loFrequency + responseVariance - (Random.value * responseVariance);
         c.hiResponseFreq = hiFrequency + responseVariance + (Random.value * responseVariance);
 
-        return c;
+		placeCell(myCell);
 
+		return myCell;
     }
+
+
+
+	private void placeCell(GameObject c)
+	{
+		float xPosition;
+		float DistFromCenter;
+		float asquared;
+		Vector3 position = new Vector3();
+
+		DistFromCenter = (Random.value - 0.5f) * 2f * radius;
+		xPosition = (Random.value - 0.5f) * 2f * radius;
+		position.x = (float)xPosition;
+		position.y = (Mathf.Abs(DistFromCenter - xPosition) / (DistFromCenter - xPosition)) * Mathf.Pow(Mathf.Abs(Mathf.Pow(DistFromCenter, 2) - Mathf.Pow(xPosition, 2)), 0.5f);
+
+		c.transform.SetPositionAndRotation(position, c.transform.localRotation);
+	}
 }
