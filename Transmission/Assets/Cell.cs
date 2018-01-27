35806148bc7f758e8896bfd6f495d80e62a1ea83
Dan Colman <danrcolman@gmail.com>
2018-01-26 20:05:22 -0800

    Initial Commit

---
+++ Transmission/Assets/Cell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+    private bool isEvil;
+    private float lowResponseFreq;
+    private float hiResponseFreq;
+    private int health = 5;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(health <= 0) {
+            // Play death animation
+            // Die, Destroy(this,1);
+        }
+
+        if(frequencty >= lowResponseFreq && frequency <= hiResponseFreq) {
+            health--;
+        }
+	}
+}
