7145112b25c710343898097335c150bcc8c7a937
timtoon <tim@timtoon.com>
2018-01-28 16:25:07 -0800

    Game Over screen and health format.

---
+++ Transmission/Assets/Scripts/LevelDisplayScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelDisplayScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject gobg = GameObject.Find("GameObject");
+        Game gobj = FindObjectOfType(typeof(Game)) as Game;
+		int level = 12;
+		GameObject.Find("Level").GetComponent<UnityEngine.UI.Text>().text = "Level\n12" + level;
+    }
+}
